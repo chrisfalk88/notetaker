2f83a20acc5c41ebf48306f5a054ef408618dca7
Chris Falk <christophersfalk@gmail.com>
2020-10-22 09:38:39 -0700

    add nodule modules. added git ignore.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+const db = require("../db/db.json");
+module.exports = function(app) {
+
+
+    app.get("/api/notes", function(req, res){
+        res.json(db)
+    })
+
+    app.post("/api/notes", function(req, res) {
+        db.push(req.body)
+    })
+
+
+}
