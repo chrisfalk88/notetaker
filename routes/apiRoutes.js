0414a3cea5d3da4c4b8d0d8998046c7e34bbe0c6
Chris Falk <christophersfalk@gmail.com>
2020-10-23 11:22:35 -0700

    added and tested fs.readfile

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,9 @@ const db = require("../db/db.json");
 const fs = require("fs");
 const uuid = require("uuid");
 
+
 module.exports = function(app) {
+
 
 
     app.get("/api/notes", function(req, res){
@@ -10,12 +12,27 @@ module.exports = function(app) {
     })
 
     app.post("/api/notes", function(req, res) {
-        db.push(req.body)
-        //call fs here? 
-        // give ids here 
-        //uuid npm --> google this 
-       // req.body.id ="FWEAj89we7032rohfawen"
-        console.log(db);
+
+        fs.readFile("db/db.json", 'utf-8', function(err, data){
+            if (err) throw err;
+            console.log(data);
+       });
+
+        // req.body.id = uuid.v4();
+        //console.log(allNotes);
+        // console.log(req.body);
+        // console.log(typeof(req.body))
+
+        
+        // db.push(req.body)
+        // console.log(db.toString());
+         //console.log(typeof(allNotes));
+
+        // fs.writeFile("db/db.json", db, function(err){
+        //     if (err) throw err
+        // })
+
+
         res.json(req.body);
         
     })
