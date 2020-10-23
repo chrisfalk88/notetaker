dd390fcfa83eb122528e7b671c0d868214f176b8
Chris Falk <christophersfalk@gmail.com>
2020-10-23 12:33:38 -0700

    completed functionality that will append db file and save changes on server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,28 +9,29 @@ module.exports = function(app) {
 
     app.get("/api/notes", function(req, res){
         res.json(db)
-    })
+    });
 
     app.post("/api/notes", function(req, res) {
+        req.body.id = uuid.v4();
+        
 
         fs.readFile("db/db.json", 'utf-8', function(err, data){
+
             if (err) throw err;
+            //
             console.log(data);
+            //readFile makes this is a string. 
+            // parse it back into a JSON object
+            let allNotes = JSON.parse(data);
+            console.log(allNotes);
+            //then we can push the req into the array
+            allNotes.push(req.body)
+            console.log(allNotes);
+
+            writetoDB(JSON.stringify(allNotes));
        });
 
-        // req.body.id = uuid.v4();
-        //console.log(allNotes);
-        // console.log(req.body);
-        // console.log(typeof(req.body))
-
-        
-        // db.push(req.body)
-        // console.log(db.toString());
-         //console.log(typeof(allNotes));
-
-        // fs.writeFile("db/db.json", db, function(err){
-        //     if (err) throw err
-        // })
+ 
 
 
         res.json(req.body);
@@ -40,9 +41,8 @@ module.exports = function(app) {
 
 }
 
-const person = {
-    firstName: "Chris",
-    lastName: "Falk",
+function writetoDB(notes) {
+    fs.writeFile("db/db.json", notes, function(err){
+        if (err) throw err
+    })
 }
-
-person.hairColor = "brown"
